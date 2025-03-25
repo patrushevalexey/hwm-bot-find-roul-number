f09f3e39dbb596ddce1189936771209952409ab8
Alexey <patrushevalexey@ya.ru>
2025-03-26 00:48:28 +0300

    smth refacrot

--- src/services/telegram/telegram.service.ts
+++ src/services/telegram/telegram.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from "@nestjs/config";
 import { Telegraf } from 'telegraf';
-
 
 @Injectable()
 export class TelegramService {
     private readonly bot: Telegraf;
 
-    constructor(private readonly configService: ConfigService) {
-        const token = this.configService.get<string>('TELEGRAM_BOT_TOKEN');
-        if (!token) {
+    constructor() {
+        const token = process.env.TELEGRAM_BOT_TOKEN;
+        if (!token)
             throw new Error('TELEGRAM_BOT_TOKEN is not defined');
-        }
         this.bot = new Telegraf(token);
     }
 
